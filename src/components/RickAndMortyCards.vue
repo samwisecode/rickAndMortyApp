6cf264dde7fe43db536865577217ca7a66e86f25
Sam Norris-Rogers <sam.norris-rogers@genexinstitute.com>
2023-06-22 11:39:49 +0200

    pagination

--- src/components/RickAndMortyCards.vue
+++ src/components/RickAndMortyCards.vue
@@ -3,25 +3,21 @@ import axios from 'axios'
 import { ref, watch } from 'vue'
 
 const characters = ref(null)
-const page = ref(0)
+const page = ref(1)
 
-const response = await  axios.get('https://rickandmortyapi.com/api/character?page=1')
-characters.value = response.data.results
+characters.value = await axios.get('https://rickandmortyapi.com/api/character?page=1')
 
 watch(page, async () => {
-    const res = await axios.get(`https://rickandmortyapi.com/api/character?page=${page.value * 20}`)
+    const res = await axios.get(`https://rickandmortyapi.com/api/character?page=${page.value}`)
+    characters.value = res.data.results
 })
 </script>
 
 <template>
     <div>
         <h1>Rick and Morty Cards</h1>
-        <div>{{ response.data.results }}</div>
-        <button @click="page = page--">
-            Previous
-        </button>
-        <button @click="page = page++">
-            Next
-        </button>
+        <button @click="page--">Previous</button>
+        <button @click="page++">Next</button>
+        <code>{{ characters }}</code>
     </div>
 </template>
