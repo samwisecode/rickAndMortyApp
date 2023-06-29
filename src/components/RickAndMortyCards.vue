811195a3e664515684929309666d7556c02dc07f
Sam Norris-Rogers <sam.norris-rogers@genexinstitute.com>
2023-06-29 19:44:07 +0200

    add episode and location info

--- src/components/RickAndMortyCards.vue
+++ src/components/RickAndMortyCards.vue
@@ -1,19 +1,24 @@
 <script setup>
 import axios from 'axios'
-import { ref, watch } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import Card from './Card.vue'
 
 const characters = ref(null)
 const page = ref(1)
 
-const response = await axios.get('https://rickandmortyapi.com/api/character?page=1')
-characters.value = [...response.data.results]
+
+onMounted(async () => {
+    const response = await axios.get('https://rickandmortyapi.com/api/character?page=1')
+    characters.value = [...response.data.results]
+})
+
 
 watch(page, async () => {
     const res = await axios.get(`https://rickandmortyapi.com/api/character?page=${page.value}`)
     characters.value = [...res.data.results]
-    
 })
+
+
 </script>
 
 <template>
@@ -25,10 +30,17 @@ watch(page, async () => {
                 :key="character.id"
                 :image="character.image"
                 :name="character.name"
-            />
+                :episode="character.episode"
+            >
+                <div>
+                    <p>{{ character.location.name }}</p>
+                    <span>{{ character.episode.length }} episode</span>
+                    <span v-if="character.episode.length > 1">s</span>
+                </div>
+            </Card>
         </div>
-        <n-button @click="page = page - 1">Previous</n-button>
-        <n-button @click="page = page + 1">Next</n-button>
+        <n-button type="info" @click="page = page - 1" :disabled="page == 1">Previous</n-button>
+        <n-button type="info" @click="page = page + 1">Next</n-button>
     </div>
 </template>
 
