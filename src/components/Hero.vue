d86f3df2c0629295796a89894ca3ab6d8a1dcbc9
Sam Norris-Rogers <sam.norris-rogers@genexinstitute.com>
2023-07-06 23:04:33 +0200

    styling updates

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+     <div class="hero">
+        <h1>Rick and Morty</h1>
+     </div>
+</template>
+
+<style scoped>
+/* Hero Styles */
+.hero {
+    height : 40vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    flex-direction: column
+}
+.hero img {
+    position: absolute;
+    height: 700px;
+    opacity: 0.1;
+}
+.hero h1 {
+    font-size: 100px;
+    font-weight: bold;
+}
+.hero p {
+    cursor: pointer;
+}
+</style>
